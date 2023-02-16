3ed3abccd005404c198bd0dc6c950914fda52bc4
painiago <95057970+painiago@users.noreply.github.com>
2023-02-15 22:20:21 -0300

    attn

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
-import { defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolte, resolve as _resolve } from 'path';
-import path from 'path';
+
+
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  baseDirectory: "/dist",
   plugins: [react()],
-  resolverr: {
-    alias: {
-      "~": path.resolve(__dirname, "node_modules"),
-      "@": path.resolve(__dirname, "src"),
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
     },
   },
-  build: {
-    chunkSizeWarningLimit: 1600,
-    commonjsOptions: {
-      include: [ /node_modules/]
-    },
-  },
-  
-});
-
-
+})
