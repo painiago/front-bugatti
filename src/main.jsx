8c3ed8dd23561bb7273d0bf24757cf6a836b99a5
painiago <95057970+painiago@users.noreply.github.com>
2023-02-16 19:37:01 -0300

    attt

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import './App.css';
 import 'bootstrap';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import "swiper/css/bundle";
-import Home from './pages/Home/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 )
