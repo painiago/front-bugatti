f3398360e0d4a550e5f409cce89436e4f18e84ac
painiago <95057970+painiago@users.noreply.github.com>
2023-02-16 20:49:40 -0300

    attt

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/dist/',
-  build: {
-    manifest: true,
-    outDir: 'assets/index',
-},
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
