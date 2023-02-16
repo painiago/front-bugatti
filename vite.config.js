21afbbc4f035619dff11abeb30bc897b26a9acd8
painiago <95057970+painiago@users.noreply.github.com>
2023-02-15 22:31:52 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/painiago.github.io/',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
@@ -11,7 +12,7 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       loader: {
-        '.js': 'jsx',
+        '.js': '.jsx',
       },
     },
   },
