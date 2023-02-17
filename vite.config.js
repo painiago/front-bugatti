7ddfc1511c5d9a4ba9585216a2872f53caa58bd1
painiago <95057970+painiago@users.noreply.github.com>
2023-02-17 16:03:23 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/front-bugatti/',
+  // base: '/front-bugatti/',
   plugins: [react()],
   resolve:{
     extensions: ['.js', '.jsx'],
