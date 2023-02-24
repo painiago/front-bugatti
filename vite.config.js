9d5f4e7a0adafd62f4648cb4bc27b495af9f9300
painiago <95057970+painiago@users.noreply.github.com>
2023-02-24 14:31:15 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-    '/@/': path.resolve(__dirname, './src')
-  }
+  alias: [
+    {find: "@", replacement: path.resolve(__dirname, 'src')}
+  ]
 })
