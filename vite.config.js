32369af9e44c19f6a72b59d397d12f3c54231930
painiago <95057970+painiago@users.noreply.github.com>
2023-02-27 14:00:30 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bugatt-fornt',
   plugins: [react()],
-  alias: [
-    { find: '@', replacement: '.' },
 
-  ]
 })
